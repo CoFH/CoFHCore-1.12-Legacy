313e291c9d7be3fc10ce059c16cb329e8a752bec
skyboy <skyboy026@gmail.com>
2014-05-06 17:02:21 -0400

    Add WorldProxy and some AT lines

---
+++ src/main/java/skyboy/core/world/WorldProxy.java
@@ -0,0 +1,56 @@
+package skyboy.core.world;
+
+import cpw.mods.fml.relauncher.ReflectionHelper;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.world.World;
+import net.minecraft.world.WorldSettings;
+import net.minecraft.world.chunk.IChunkProvider;
+
+public class WorldProxy extends World {
+	protected World proxiedWorld;
+
+	private static String getPar2String(World world) {
+		return world.getWorldInfo().getWorldName();
+	}
+
+	private static WorldSettings getPar4WorldSettings(World world) {
+		return new WorldSettings(world.getWorldInfo());
+	}
+	
+	public WorldProxy(World world) {
+		super(world.getSaveHandler(), getPar2String(world), world.provider, getPar4WorldSettings(world), world.theProfiler);
+		this.proxiedWorld = world;
+		//perWorldStorage = world.perWorldStorage; // final, set in super; requires reflection
+		ReflectionHelper.setPrivateValue(World.class, this, world.perWorldStorage, new String[]{"perWorldStorage"}); // forge-added, no reobf
+		scheduledUpdatesAreImmediate = world.scheduledUpdatesAreImmediate;
+		loadedEntityList = world.loadedEntityList;
+		loadedTileEntityList = world.loadedTileEntityList;
+		playerEntities = world.playerEntities;
+		weatherEffects = world.weatherEffects;
+		skylightSubtracted = world.skylightSubtracted;
+		prevRainingStrength = world.prevRainingStrength;
+		rainingStrength = world.rainingStrength;
+		prevThunderingStrength = world.prevThunderingStrength;
+		thunderingStrength = world.thunderingStrength;
+		lastLightningBolt = world.lastLightningBolt;
+		difficultySetting = world.difficultySetting;
+		rand = world.rand;
+		//provider = world.provider; // handled by super
+		findingSpawnPoint = world.findingSpawnPoint;
+		mapStorage = world.mapStorage;
+		villageCollectionObj = world.villageCollectionObj;
+		//theProfiler = world.theProfiler; // handled by super
+		isRemote = world.isRemote;
+	}
+
+	@Override
+	protected IChunkProvider createChunkProvider() {
+		return null;
+	}
+
+	@Override
+	public Entity getEntityByID(int var1) {
+		return null;
+	}
+}
---
+++ src/main/java/skyboy/core/world/WorldServerProxy.java
@@ -0,0 +1,53 @@
+package skyboy.core.world;
+
+import cpw.mods.fml.relauncher.ReflectionHelper;
+
+import net.minecraft.world.World;
+import net.minecraft.world.WorldServer;
+import net.minecraft.world.WorldSettings;
+
+public class WorldServerProxy extends WorldServer {    
+	protected WorldServer proxiedWorld;
+	
+	/*  TODO: override all methods of super using an ASM transformer
+	 *  instead of keeping it updated from version to version
+	 */
+
+	private static String getPar2String(World world) {
+		return world.getWorldInfo().getWorldName();
+	}
+
+	private static WorldSettings getPar4WorldSettings(World world) {
+		return new WorldSettings(world.getWorldInfo());
+	}
+	
+	public WorldServerProxy(WorldServer world) {
+		super(world.func_73046_m(), world.getSaveHandler(), getPar2String(world), world.provider, getPar4WorldSettings(world), world.theProfiler);
+		this.proxiedWorld = world;
+		//perWorldStorage = world.perWorldStorage; // final, set in super; requires reflection
+		ReflectionHelper.setPrivateValue(World.class, this, world.perWorldStorage, new String[]{"perWorldStorage"}); // forge-added, no reobf
+		scheduledUpdatesAreImmediate = world.scheduledUpdatesAreImmediate;
+		loadedEntityList = world.loadedEntityList;
+		loadedTileEntityList = world.loadedTileEntityList;
+		playerEntities = world.playerEntities;
+		weatherEffects = world.weatherEffects;
+		skylightSubtracted = world.skylightSubtracted;
+		prevRainingStrength = world.prevRainingStrength;
+		rainingStrength = world.rainingStrength;
+		prevThunderingStrength = world.prevThunderingStrength;
+		thunderingStrength = world.thunderingStrength;
+		lastLightningBolt = world.lastLightningBolt;
+		difficultySetting = world.difficultySetting;
+		rand = world.rand;
+		//provider = world.provider; // handled by super
+		findingSpawnPoint = world.findingSpawnPoint;
+		mapStorage = world.mapStorage;
+		villageCollectionObj = world.villageCollectionObj;
+		//theProfiler = world.theProfiler; // handled by super
+		isRemote = world.isRemote;
+		theChunkProviderServer = world.theChunkProviderServer;
+		levelSaving = world.levelSaving;
+	    allPlayersSleeping = world.allPlayersSleeping;
+		customTeleporters = world.customTeleporters;
+	}
+}
