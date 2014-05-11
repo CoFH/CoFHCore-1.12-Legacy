ead327193db8f3598d0b23888cd5e32cd44dee2f
skyboy <skyboy026@gmail.com>
2014-05-11 02:37:54 -0400

    Update RegistryUtils

--- src/main/java/cofh/util/RegistryUtils.java
+++ src/main/java/cofh/util/RegistryUtils.java
@@ -9,6 +9,9 @@ public class RegistryUtils {
 	@SuppressWarnings({ "rawtypes", "unchecked" })
 	public static void overwriteEntry(RegistryNamespaced registry, String name, Object object) {
 		
+		Object oldThing = registry.getObject(name);
+		int id = registry.getIDForObject(oldThing);
 		((BiMap)registry.registryObjects).forcePut(name, object);
+		registry.underlyingIntegerMap.func_148746_a(object, id);
 	}
 }
