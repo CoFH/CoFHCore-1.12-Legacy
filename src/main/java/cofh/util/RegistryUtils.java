f718b766b5c1749d99ad23f0712fcc2f3bd7b27f
skyboy <skyboy026@gmail.com>
2014-05-14 23:22:08 -0400

    Add texture check to RegistryUtils

--- src/main/java/cofh/util/RegistryUtils.java
+++ src/main/java/cofh/util/RegistryUtils.java
@@ -1,8 +1,12 @@
 package cofh.util;
 
 import com.google.common.collect.BiMap;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 
+import net.minecraft.client.Minecraft;
 import net.minecraft.util.RegistryNamespaced;
+import net.minecraft.util.ResourceLocation;
 
 public class RegistryUtils {
 
@@ -15,5 +19,43 @@ public class RegistryUtils {
 		registry.underlyingIntegerMap.func_148746_a(object, id);
 		map.remove(name);
 		map.forcePut(name, object);
+	}
+	
+	@SideOnly(Side.CLIENT)
+	public static boolean textureExists(String texture)
+	{
+		
+		try {
+			
+			Minecraft.getMinecraft().getResourceManager().getAllResources(new ResourceLocation(texture));
+			return true;
+		} catch (Throwable _) {
+			
+			return false;
+		}
+	}
+	
+	@SideOnly(Side.CLIENT)
+	public static boolean itemTextureExists(String texture)
+	{
+		
+		int i = texture.indexOf(':'); 
+		if (i > 0)
+			texture = texture.substring(0, i) + ":textures/items/" + texture.substring(i + 1, texture.length());
+		else
+			texture = "textures/items/" + texture;
+		return textureExists(texture);
+	}
+	
+	@SideOnly(Side.CLIENT)
+	public static boolean blockTextureExists(String texture)
+	{
+		
+		int i = texture.indexOf(':'); 
+		if (i > 0)
+			texture = texture.substring(0, i) + ":textures/blocks/" + texture.substring(i + 1, texture.length());
+		else
+			texture = "textures/items/" + texture;
+		return textureExists(texture);
 	}
 }
