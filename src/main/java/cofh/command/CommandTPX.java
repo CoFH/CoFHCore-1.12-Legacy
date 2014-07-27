674048cec7771e99f4e820a34a03fd0bcb53a78c
skyboy <skyboy026@gmail.com>
2014-07-27 09:02:20 -0400

    Cleanup tpx, extend & unify functionality

--- src/main/java/cofh/command/CommandTPX.java
+++ src/main/java/cofh/command/CommandTPX.java
@@ -1,5 +1,7 @@
 package cofh.command;
 
+import cofh.util.StringHelper;
+
 import java.util.List;
 
 import net.minecraft.command.CommandBase;
@@ -9,85 +11,163 @@ import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.ChatComponentText;
 import net.minecraftforge.common.DimensionManager;
-import cofh.util.StringHelper;
 
 public class CommandTPX implements ISubCommand {
-    
-    public static CommandTPX instance = new CommandTPX();
-    
-    @Override
-    public String getCommandName() {
-        
-        return "tpx";
-    }
-    
-    @Override
-    public void handleCommand(ICommandSender sender, String[] arguments) {
-        
-        switch(arguments.length){
-        
-        case 0: sender.addChatMessage(new ChatComponentText("Invalid Syntax. /cofh tpx " + StringHelper.PINK + "[username] " + StringHelper.YELLOW + "<x> <y> <z> <dimension id>")); break;
-        case 1: sender.addChatMessage(new ChatComponentText("Invalid Syntax. /cofh tpx " + StringHelper.PINK + "[username] " + StringHelper.YELLOW + "<x> <y> <z> <dimension id>")); break;
-        case 2:
-            EntityPlayerMP player = CommandBase.getPlayer(sender, arguments[1]);
-            EntityPlayerMP playerSender = CommandBase.getCommandSenderAsPlayer(sender);
-            if(!player.equals(playerSender)){
-                player.mountEntity((Entity)null);
-                if(playerSender.dimension == player.dimension){
-                    playerSender.setPositionAndUpdate(player.posX, player.posY, player.posZ);
-                }else{
-                    playerSender.mcServer.getConfigurationManager().transferPlayerToDimension(player, player.dimension);
-                    playerSender.setPositionAndUpdate(player.posX, player.posY, player.posZ);
-                }
-            }else{
-                sender.addChatMessage(new ChatComponentText("Teleport to somebody besides yourself!"));
-            }
-            break;
-        case 3: sender.addChatMessage(new ChatComponentText("Invalid Syntax. /cofh tpx " + StringHelper.PINK + "[username] " + StringHelper.YELLOW + "<x> <y> <z> <dimension id>")); break;
-        case 4: sender.addChatMessage(new ChatComponentText("Invalid Syntax. /cofh tpx " + StringHelper.PINK + "[username] " + StringHelper.YELLOW + "<x> <y> <z> <dimension id>")); break;
-        case 5:
-            player = CommandBase.getPlayer(sender, arguments[1]);
-            player.mountEntity((Entity)null);
-            player.setPositionAndUpdate(CommandBase.func_110666_a(player, player.posX, arguments[2]), CommandBase.func_110666_a(player, player.posY, arguments[3]), CommandBase.func_110666_a(player, player.posZ, arguments[4]));
-            break;
-        default:
-            player = CommandBase.getPlayer(sender, arguments[1]);
-            int dimension = CommandBase.parseInt(sender, arguments[5]);
-            
-            if(!DimensionManager.isDimensionRegistered(dimension)){
-                sender.addChatMessage(new ChatComponentText(StringHelper.RED + "That dimension does not exist")); break;
-            }
-            
-            if(player.dimension != dimension){
-                player.mcServer.getConfigurationManager().transferPlayerToDimension(player, dimension);
-            }
-            player.mountEntity((Entity)null);
-            player.setPositionAndUpdate(CommandBase.func_110666_a(player, player.posX, arguments[2]), CommandBase.func_110666_a(player, player.posY, arguments[3]), CommandBase.func_110666_a(player, player.posZ, arguments[4]));
-            break;
-        }
-    }
-    
-    @SuppressWarnings("unchecked")
-    @Override
-    public List<String> addTabCompletionOptions(ICommandSender sender, String[] args) {
-        
-        if(args.length == 2){
-            return CommandBase.getListOfStringsMatchingLastWord(args, MinecraftServer.getServer().getAllUsernames()); 
-        }else if(args.length >= 6){
-            
-            Integer[] ids = DimensionManager.getIDs();
-            String[] strings = new String[ids.length];
-            
-            for(int i = 0; i < ids.length; i++){
-                strings[i] = ids[i].toString();
-            }
-            
-            return CommandBase.getListOfStringsMatchingLastWord(args, strings); 
-        }
-        
-        return null;
-        
-    }
-    
+
+	public static CommandTPX instance = new CommandTPX();
+
+	@Override
+	public String getCommandName() {
+
+		return "tpx";
+	}
+
+	@Override
+	public void handleCommand(ICommandSender sender, String[] arguments) {
+		
+		// TODO: allow selector commands to target anything (single player, all players[@a], specific entities [@e], etc.)
+		// where it makes sense to allow it (e.g., not allowing teleporting a single thing to many things)
+		
+		// TODO: move the player before teleporting? noticed odd offsets when moving after
+
+		switch (arguments.length) {
+
+		case 0: // () ???? how did we get here again?
+		case 1: // (tpx) invalid command
+			sender.addChatMessage(new ChatComponentText("Invalid Syntax. /cofh tpx " + StringHelper.PINK + "[username] " + StringHelper.YELLOW + "[<x> <y> <z>] <dimension id>"));
+			break;
+		case 2: // (tpx {<player>|<dimension>}) teleporting player to self, or self to dimension
+			EntityPlayerMP playerSender = CommandBase.getCommandSenderAsPlayer(sender);
+			try {
+				EntityPlayerMP player = CommandBase.getPlayer(sender, arguments[1]);
+				if (!player.equals(playerSender)) {
+					player.mountEntity((Entity)null);
+					if (playerSender.dimension == player.dimension) {
+						player.setPositionAndUpdate(playerSender.posX, playerSender.posY, playerSender.posZ);
+					} else {
+						playerSender.mcServer.getConfigurationManager().transferPlayerToDimension(player, playerSender.dimension);
+						player.setPositionAndUpdate(playerSender.posX, playerSender.posY, playerSender.posZ);
+					}
+				} else {
+					sender.addChatMessage(new ChatComponentText("Sucessfully teleported you to yourself!"));
+				}
+				break;
+			} catch (Throwable t) {
+				int dimension;
+				try {
+					dimension = Integer.parseInt(arguments[1]);
+				} catch (Throwable p) { // not a number, assume they wanted a player
+					if (t instanceof RuntimeException)
+						throw (RuntimeException)t; // player error is
+					throw new RuntimeException(t);
+				}
+
+				if (!DimensionManager.isDimensionRegistered(dimension)) {
+					sender.addChatMessage(new ChatComponentText(StringHelper.RED + "That dimension does not exist."));
+					break;
+				}
+
+				DimensionManager.initDimension(dimension);
+				double f = DimensionManager.getProvider(playerSender.dimension).getMovementFactor();
+				f /= DimensionManager.getProvider(dimension).getMovementFactor();
+
+				playerSender.mountEntity((Entity)null);
+				if (playerSender.dimension != dimension) {
+					playerSender.mcServer.getConfigurationManager().transferPlayerToDimension(playerSender, dimension);
+				}
+				playerSender.setPositionAndUpdate(playerSender.posX * f, playerSender.posY, playerSender.posZ * f);
+			}
+			break;
+		case 3: // (tpx <player> {<player>|<dimension>}) teleporting player to player or player to dimension
+			EntityPlayerMP player = CommandBase.getPlayer(sender, arguments[1]);
+			try {
+				EntityPlayerMP otherPlayer = CommandBase.getPlayer(sender, arguments[1]);
+				if (!player.equals(otherPlayer)) {
+					player.mountEntity((Entity)null);
+					if (otherPlayer.dimension == player.dimension) {
+						player.setPositionAndUpdate(otherPlayer.posX, otherPlayer.posY, otherPlayer.posZ);
+					} else {
+						otherPlayer.mcServer.getConfigurationManager().transferPlayerToDimension(player, otherPlayer.dimension);
+						player.setPositionAndUpdate(otherPlayer.posX, otherPlayer.posY, otherPlayer.posZ);
+					}
+				} else {
+					sender.addChatMessage(new ChatComponentText("Sucessfully teleported " + arguments[1] + " to themself!"));
+				}
+				break;
+			} catch (Throwable t) {
+				int dimension;
+				try {
+					dimension = Integer.parseInt(arguments[1]);
+				} catch (Throwable p) { // not a number, assume they wanted a player
+					if (t instanceof RuntimeException)
+						throw (RuntimeException)t; // player error is
+					throw new RuntimeException(t);
+				}
+	
+				if (!DimensionManager.isDimensionRegistered(dimension)) {
+					sender.addChatMessage(new ChatComponentText(StringHelper.RED + "That dimension does not exist"));
+					break;
+				}
+				DimensionManager.initDimension(dimension);
+				double f = DimensionManager.getProvider(player.dimension).getMovementFactor();
+				f /= DimensionManager.getProvider(dimension).getMovementFactor();
+				
+				player.mountEntity((Entity)null);
+				if (player.dimension != dimension) {
+					player.mcServer.getConfigurationManager().transferPlayerToDimension(player, dimension);
+				}
+				player.setPositionAndUpdate(player.posX * f, player.posY, player.posZ * f);
+			}
+			break;
+		case 4: // (tpx <x> <y> <z>) teleporting self within dimension
+			playerSender = CommandBase.getCommandSenderAsPlayer(sender);
+			playerSender.setPositionAndUpdate(CommandBase.func_110666_a(playerSender, playerSender.posX, arguments[2]), CommandBase.func_110666_a(playerSender, playerSender.posY, arguments[3]), CommandBase.func_110666_a(playerSender, playerSender.posZ, arguments[4]));
+			break;
+		case 5: // (tpx <player> <x> <y> <z>) teleporting player within player's dimension
+			player = CommandBase.getPlayer(sender, arguments[1]);
+			player.mountEntity((Entity)null);
+			player.setPositionAndUpdate(CommandBase.func_110666_a(player, player.posX, arguments[2]), CommandBase.func_110666_a(player, player.posY, arguments[3]), CommandBase.func_110666_a(player, player.posZ, arguments[4]));
+			break;
+		case 6: // (tpx <player> <x> <y> <z> <dimension>) teleporting player to dimension and location
+		default: // ignore excess tokens. warn?
+			player = CommandBase.getPlayer(sender, arguments[1]);
+			int dimension = CommandBase.parseInt(sender, arguments[5]);
+
+			if (!DimensionManager.isDimensionRegistered(dimension)) {
+				sender.addChatMessage(new ChatComponentText(StringHelper.RED + "That dimension does not exist"));
+				break;
+			}
+
+			player.mountEntity((Entity)null);
+			if (player.dimension != dimension) {
+				player.mcServer.getConfigurationManager().transferPlayerToDimension(player, dimension);
+			}
+			player.setPositionAndUpdate(CommandBase.func_110666_a(player, player.posX, arguments[2]), CommandBase.func_110666_a(player, player.posY, arguments[3]), CommandBase.func_110666_a(player, player.posZ, arguments[4]));
+			break;
+		}
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<String> addTabCompletionOptions(ICommandSender sender, String[] args) {
+
+		if(args.length == 2){
+			return CommandBase.getListOfStringsMatchingLastWord(args, MinecraftServer.getServer().getAllUsernames()); 
+		}else if(args.length >= 6){
+
+			Integer[] ids = DimensionManager.getIDs();
+			String[] strings = new String[ids.length];
+
+			for(int i = 0; i < ids.length; i++){
+				strings[i] = ids[i].toString();
+			}
+
+			return CommandBase.getListOfStringsMatchingLastWord(args, strings); 
+		}
+
+		return null;
+
+	}
+
 }
 
