68b09eb27422b76e73bb746472cfb35b7da22540
skyboy <skyboy026@gmail.com>
2015-01-14 05:14:22 -0500

    Extend ItemArmorAdv

--- src/main/java/cofh/core/item/ItemArmorAdv.java
+++ src/main/java/cofh/core/item/ItemArmorAdv.java
@@ -1,8 +1,13 @@
 package cofh.core.item;
 
 import cofh.lib.util.helpers.ItemHelper;
+import com.google.common.collect.HashMultimap;
+import com.google.common.collect.Multimap;
+
+import java.util.Collection;
 
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.ai.attributes.AttributeModifier;
 import net.minecraft.item.ItemArmor;
 import net.minecraft.item.ItemStack;
 
@@ -10,6 +15,7 @@ public class ItemArmorAdv extends ItemArmor {
 
 	public String repairIngot = "";
 	public String[] textures = new String[2];
+	protected Multimap<String, AttributeModifier> properties = HashMultimap.create();
 
 	public ItemArmorAdv(ArmorMaterial material, int type) {
 
@@ -41,6 +47,26 @@ public class ItemArmorAdv extends ItemArmor {
 			return textures[1];
 		}
 		return textures[0];
+	}
+
+	@SuppressWarnings("rawtypes")
+	@Override
+	public Multimap getAttributeModifiers(ItemStack stack) {
+
+		Multimap<String, AttributeModifier> map = super.getAttributeModifiers(stack);
+		map.putAll(properties);
+		return map;
+	}
+
+	public ItemArmorAdv putAttribute(String attribute, AttributeModifier modifier) {
+
+		properties.put(attribute, modifier);
+		return this;
+	}
+
+	public Collection<AttributeModifier> removeAttribute(String attribute) {
+
+		return properties.removeAll(attribute);
 	}
 
 }
