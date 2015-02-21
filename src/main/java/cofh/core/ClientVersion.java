0c81c370c7df1955eb46a41b41d0b808e9a720af
skyboy <skyboy026@gmail.com>
2015-02-21 18:04:43 -0500

    Fix dependency string

--- src/main/java/cofh/core/ClientVersion.java
+++ src/main/java/cofh/core/ClientVersion.java
@@ -6,7 +6,7 @@ import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 
 @SideOnly(Side.CLIENT)
-@Mod(modid = "CoFHCoreClient", name = CoFHCore.modName, version = CoFHCore.version, dependencies = "Forge@[10.13.2.1291,10.14)")
+@Mod(modid = "CoFHCoreClient", name = CoFHCore.modName, version = CoFHCore.version, dependencies = "required-after:Forge@[10.13.2.1291,10.14)")
 public class ClientVersion {
 
 }
