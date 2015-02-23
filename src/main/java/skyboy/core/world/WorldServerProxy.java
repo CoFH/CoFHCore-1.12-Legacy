ffbfe99e1f6a6e81e44a6bf0707b8d4d76ebdafa
skyboy <skyboy026@gmail.com>
2015-02-23 08:39:03 -0500

    Add method to WorldServerProxy for updating primatives

--- src/main/java/skyboy/core/world/WorldServerProxy.java
+++ src/main/java/skyboy/core/world/WorldServerProxy.java
@@ -25,30 +25,35 @@ public abstract class WorldServerProxy extends WorldServerShim {
 		super(world.func_73046_m(), world.getSaveHandler(), getWorldName(world), world.provider, getWorldSettings(world), world.theProfiler);
 		this.proxiedWorld = world;
 		// perWorldStorage = world.perWorldStorage; // final, set in super; requires reflection
-		ReflectionHelper.setPrivateValue(World.class, this, world.perWorldStorage, new String[] { "perWorldStorage" }); // forge-added, no reobf
-		scheduledUpdatesAreImmediate = world.scheduledUpdatesAreImmediate;
+		ReflectionHelper.setPrivateValue(World.class, this, world.perWorldStorage, "perWorldStorage"); // forge-added, no reobf
 		loadedEntityList = world.loadedEntityList;
 		loadedTileEntityList = world.loadedTileEntityList;
 		playerEntities = world.playerEntities;
 		weatherEffects = world.weatherEffects;
-		skylightSubtracted = world.skylightSubtracted;
-		prevRainingStrength = world.prevRainingStrength;
-		rainingStrength = world.rainingStrength;
-		prevThunderingStrength = world.prevThunderingStrength;
-		thunderingStrength = world.thunderingStrength;
-		lastLightningBolt = world.lastLightningBolt;
-		difficultySetting = world.difficultySetting;
 		rand = world.rand;
 		// provider = world.provider; // handled by super
-		findingSpawnPoint = world.findingSpawnPoint;
 		mapStorage = world.mapStorage;
 		villageCollectionObj = world.villageCollectionObj;
 		// theProfiler = world.theProfiler; // handled by super
 		isRemote = world.isRemote;
-		theChunkProviderServer = world.theChunkProviderServer;
-		levelSaving = world.levelSaving;
-		// allPlayersSleeping = world.allPlayersSleeping;
 		customTeleporters = world.customTeleporters;
+		cofh_updateProps();
+	}
+
+	protected void cofh_updateProps() {
+
+		scheduledUpdatesAreImmediate = proxiedWorld.scheduledUpdatesAreImmediate;
+		skylightSubtracted = proxiedWorld.skylightSubtracted;
+		prevRainingStrength = proxiedWorld.prevRainingStrength;
+		rainingStrength = proxiedWorld.rainingStrength;
+		prevThunderingStrength = proxiedWorld.prevThunderingStrength;
+		thunderingStrength = proxiedWorld.thunderingStrength;
+		lastLightningBolt = proxiedWorld.lastLightningBolt;
+		difficultySetting = proxiedWorld.difficultySetting;
+		findingSpawnPoint = proxiedWorld.findingSpawnPoint;
+		theChunkProviderServer = proxiedWorld.theChunkProviderServer;
+		allPlayersSleeping = proxiedWorld.allPlayersSleeping;
+		levelSaving = proxiedWorld.levelSaving;
 	}
 
 }
