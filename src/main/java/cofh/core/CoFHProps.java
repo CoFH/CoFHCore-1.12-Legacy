e4d130f68deea57e1cbcba8d9fa94ee295ec0909
King Lemming <kinglemming@gmail.com>
2015-03-03 18:47:43 -0500

    Update to RC3.
    Signed-off-by: King Lemming <kinglemming@gmail.com>

--- src/main/java/cofh/core/CoFHProps.java
+++ src/main/java/cofh/core/CoFHProps.java
@@ -14,7 +14,7 @@ public class CoFHProps {
 
 	}
 
-	public static final String VERSION = "1.7.10R3.0.0RC2";
+	public static final String VERSION = "1.7.10R3.0.0RC3";
 
 	private static final String BUILD = "1217";
 	public static final String FML_REQ = "7.10.0." + BUILD;
