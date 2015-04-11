8f446a5902a058982c75e7a0773fa6ee721d260b
King Lemming <kinglemming@gmail.com>
2015-04-11 15:10:16 -0400

    Increase version.
    Signed-off-by: King Lemming <kinglemming@gmail.com>

--- src/main/java/cofh/core/CoFHProps.java
+++ src/main/java/cofh/core/CoFHProps.java
@@ -14,7 +14,7 @@ public class CoFHProps {
 
 	}
 
-	public static final String VERSION = "1.7.10R3.0.0";
+	public static final String VERSION = "1.7.10R3.0.1";
 
 	private static final String BUILD = "1217";
 	public static final String FML_REQ = "7.10.0." + BUILD;
