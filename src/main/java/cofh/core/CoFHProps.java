2db51f85cde4779d20f33fbc52cc4b27bd2c952f
skyboy <skyboy026@gmail.com>
2015-05-16 13:47:31 -0400

    Remove FML dep

--- src/main/java/cofh/core/CoFHProps.java
+++ src/main/java/cofh/core/CoFHProps.java
@@ -17,12 +17,10 @@ public class CoFHProps {
 	public static final String VERSION = "1.7.10R3.0.2";
 
 	private static final String BUILD = "1291";
-	public static final String FML_REQ = "7.10.0." + BUILD;
-	public static final String FML_REQ_MAX = "7.11";
 	public static final String FORGE_REQ = "10.13.1." + BUILD;
 	public static final String FORGE_REQ_MAX = "10.14";
 
-	public static final String DEPENDENCIES = "required-after:FML@[" + CoFHProps.FML_REQ + "," + CoFHProps.FML_REQ_MAX + ");" + "required-after:Forge@["
+	public static final String DEPENDENCIES = "required-after:required-after:Forge@["
 			+ CoFHProps.FORGE_REQ + "," + CoFHProps.FORGE_REQ_MAX + ");";
 
 	public static final String DOWNLOAD_URL = "http://teamcofh.com/downloads/";
