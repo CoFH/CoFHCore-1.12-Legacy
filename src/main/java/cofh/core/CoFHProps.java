c7af51417eb356a8388705be5cdf6cfd10fb0c19
King Lemming <kinglemming@gmail.com>
2015-07-04 18:03:20 -0400

    Increment Version number. Beta.
    Signed-off-by: King Lemming <kinglemming@gmail.com>

--- src/main/java/cofh/core/CoFHProps.java
+++ src/main/java/cofh/core/CoFHProps.java
@@ -14,7 +14,7 @@ public class CoFHProps {
 
 	}
 
-	public static final String VERSION = "1.7.10R3.0.2";
+	public static final String VERSION = "1.7.10R3.0.3B1";
 
 	private static final String BUILD = "1291";
 	public static final String FORGE_REQ = "10.13.1." + BUILD;
