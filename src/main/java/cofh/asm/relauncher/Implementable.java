7a53cb981ec3a896514fd95ed0d2b7075293d150
skyboy <mr.wraith.06@gmail.com>
2015-10-13 01:19:45 -0400

    Add javadoc for @Implementable

--- src/main/java/cofh/asm/relauncher/Implementable.java
+++ src/main/java/cofh/asm/relauncher/Implementable.java
@@ -1,10 +1,15 @@
 package cofh.asm.relauncher;
 
+import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Adds an interface to the class when the interface is present at runtime for soft dependencies.
+ */
+@Documented
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ ElementType.TYPE })
 public @interface Implementable {
--- src/main/java/cofh/asm/relauncher/Strippable.java
+++ src/main/java/cofh/asm/relauncher/Strippable.java
@@ -10,8 +10,9 @@ import java.lang.annotation.Target;
  * When used on a class, methods from referenced interfaces will not be removed <br>
  * When using this annotation on methods, ensure you do not switch on an enum inside that method. JavaC implementation details means this will cause crashes.
  * <p>
- * Can also strip on modid using "mod:CoFHCore" as a value <br>
- * Can also strip on API using "api:CoFHAPI|energy" as a value
+ * Takes a class name as the value. e.g., "cofh.lib.network.ByteBufHelper" <br>
+ * Can also strip on modid using e.g., "mod:CoFHCore" as a value <br>
+ * Can also strip on API using e.g., "api:CoFHAPI|energy" as a value
  */
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
