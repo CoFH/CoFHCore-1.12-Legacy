54bf1d8f1a347679ed5f96adfda776cb30a492b1
King Lemming <kinglemming@gmail.com>
2017-03-04 15:55:56 -0500

    Refactors >.>
    Signed-off-by: King Lemming <kinglemming@gmail.com>

--- src/main/java/cofh/asm/hooks/ASMHooks.java
+++ src/main/java/cofh/asm/hooks/ASMHooks.java
@@ -5,14 +5,14 @@ import net.minecraftforge.common.MinecraftForge;
 
 public class ASMHooks {
 
-    public static void preGenerateWorld(World world, int chunkX, int chunkZ) {
+	public static void preGenerateWorld(World world, int chunkX, int chunkZ) {
 
-        MinecraftForge.EVENT_BUS.post(new ModPopulateChunkEvent.Pre(world, chunkX, chunkZ));
-    }
+		MinecraftForge.EVENT_BUS.post(new ModPopulateChunkEvent.Pre(world, chunkX, chunkZ));
+	}
 
-    public static void postGenerateWorld(World world, int chunkX, int chunkZ) {
+	public static void postGenerateWorld(World world, int chunkX, int chunkZ) {
 
-        MinecraftForge.EVENT_BUS.post(new ModPopulateChunkEvent.Post(world, chunkX, chunkZ));
-    }
+		MinecraftForge.EVENT_BUS.post(new ModPopulateChunkEvent.Post(world, chunkX, chunkZ));
+	}
 
 }
--- src/main/java/cofh/asm/hooks/ModPopulateChunkEvent.java
+++ src/main/java/cofh/asm/hooks/ModPopulateChunkEvent.java
@@ -1,6 +1,5 @@
 package cofh.asm.hooks;
 
-
 import net.minecraft.world.World;
 import net.minecraftforge.fml.common.eventhandler.Event;
 
