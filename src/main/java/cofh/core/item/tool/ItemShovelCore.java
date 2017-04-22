6e6b5afaf2d5ab335a37b4af74ccc007b480606f
King Lemming <kinglemming@gmail.com>
2017-04-22 03:05:30 -0400

    Shovels can now pack grass to paths.
    Signed-off-by: King Lemming <kinglemming@gmail.com>

--- src/main/java/cofh/core/item/tool/ItemShovelCore.java
+++ src/main/java/cofh/core/item/tool/ItemShovelCore.java
@@ -1,7 +1,19 @@
 package cofh.core.item.tool;
 
+import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Blocks;
+import net.minecraft.init.SoundEvents;
 import net.minecraft.item.ItemSpade;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumActionResult;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.SoundCategory;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 
 public class ItemShovelCore extends ItemToolCore {
 
@@ -18,6 +30,28 @@ public class ItemShovelCore extends ItemToolCore {
 		effectiveMaterials.add(Material.SNOW);
 		effectiveMaterials.add(Material.CRAFTED_SNOW);
 		effectiveMaterials.add(Material.CLAY);
+	}
+
+	public EnumActionResult onItemUse(ItemStack stack, EntityPlayer playerIn, World worldIn, BlockPos pos, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) {
+
+		if (!playerIn.canPlayerEdit(pos.offset(facing), facing, stack)) {
+			return EnumActionResult.FAIL;
+		} else {
+			IBlockState state = worldIn.getBlockState(pos);
+			Block block = state.getBlock();
+
+			if (facing != EnumFacing.DOWN && worldIn.getBlockState(pos.up()).getMaterial() == Material.AIR && block == Blocks.GRASS) {
+				worldIn.playSound(playerIn, pos, SoundEvents.ITEM_SHOVEL_FLATTEN, SoundCategory.BLOCKS, 1.0F, 1.0F);
+
+				if (!worldIn.isRemote) {
+					worldIn.setBlockState(pos, Blocks.GRASS_PATH.getDefaultState(), 11);
+					stack.damageItem(1, playerIn);
+				}
+				return EnumActionResult.SUCCESS;
+			} else {
+				return EnumActionResult.PASS;
+			}
+		}
 	}
 
 }
