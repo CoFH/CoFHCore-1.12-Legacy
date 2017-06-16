a20a715ce1362f35a1002961d388ce6e82b9aa5b
King Lemming <kinglemming@gmail.com>
2017-06-16 08:10:19 -0400

    Update Fishing Rod. Needs ASM patch or Forge fix.
    Signed-off-by: King Lemming <kinglemming@gmail.com>

--- src/main/java/cofh/core/item/tool/ItemFishingRodCore.java
+++ src/main/java/cofh/core/item/tool/ItemFishingRodCore.java
@@ -1,16 +1,17 @@
 package cofh.core.item.tool;
 
-import cofh.core.entity.EntityFishHookCore;
 import cofh.lib.util.helpers.ItemHelper;
-import cofh.lib.util.helpers.ServerHelper;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.projectile.EntityFishHook;
 import net.minecraft.init.SoundEvents;
 import net.minecraft.item.IItemPropertyGetter;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemFishingRod;
 import net.minecraft.item.ItemStack;
+import net.minecraft.stats.StatList;
 import net.minecraft.util.*;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.relauncher.Side;
@@ -36,6 +37,7 @@ public class ItemFishingRodCore extends ItemFishingRod {
 		setMaxDamage(toolMaterial.getMaxUses() + 5);
 
 		addPropertyOverride(new ResourceLocation("cast"), new IItemPropertyGetter() {
+
 			@SideOnly (Side.CLIENT)
 			public float apply(ItemStack stack, @Nullable World worldIn, @Nullable EntityLivingBase entityIn) {
 
@@ -95,23 +97,73 @@ public class ItemFishingRodCore extends ItemFishingRod {
 		return toolMaterial.getEnchantability();
 	}
 
-	@Override
-	public ActionResult<ItemStack> onItemRightClick(World world, EntityPlayer player, EnumHand hand) {
+//	@Override
+//	public ActionResult<ItemStack> onItemRightClick(World world, EntityPlayer player, EnumHand hand) {
+//
+//		ItemStack stack = player.getHeldItem(hand);
+//
+//		if (player.fishEntity != null) {
+//			int i = player.fishEntity.handleHookRetraction();
+//			stack.damageItem(i, player);
+//			player.swingArm(hand);
+//		} else {
+//			world.playSound( null, player.posX, player.posY, player.posZ, SoundEvents.ENTITY_BOBBER_THROW, SoundCategory.NEUTRAL, 0.5F, 0.4F / (itemRand.nextFloat() * 0.4F + 0.8F));
+//
+//			if (ServerHelper.isServerWorld(world)) {
+//				EntityFishHook hook = new EntityFishHook(world, player);
+//
+//				int enchantSpeed = EnchantmentHelper.func_191528_c(stack);
+//				hook.func_191516_a(speedModifier + enchantSpeed);
+//
+//				int enchantLuck = EnchantmentHelper.func_191529_b(stack);
+//				hook.func_191517_b(luckModifier + enchantLuck);
+//
+//				world.spawnEntity(hook);
+//			}
+//			player.swingArm(hand);
+//		}
+//		return new ActionResult<>(EnumActionResult.SUCCESS, stack);
+//	}
 
-		ItemStack stack = player.getHeldItem(hand);
-		if (player.fishEntity != null) {
-			int i = player.fishEntity.handleHookRetraction();
-			stack.damageItem(i, player);
-			player.swingArm(hand);
-		} else {
-			world.playSound(null, player.posX, player.posY, player.posZ, SoundEvents.ENTITY_BOBBER_THROW, SoundCategory.NEUTRAL, 0.5F, 0.4F / (itemRand.nextFloat() * 0.4F + 0.8F));
+	public ActionResult<ItemStack> onItemRightClick(World worldIn, EntityPlayer playerIn, EnumHand handIn)
+	{
+		ItemStack itemstack = playerIn.getHeldItem(handIn);
 
-			if (ServerHelper.isServerWorld(world)) {
-				world.spawnEntity(new EntityFishHookCore(world, player, luckModifier, speedModifier));
-			}
-			player.swingArm(hand);
+		if (playerIn.fishEntity != null)
+		{
+			int i = playerIn.fishEntity.handleHookRetraction();
+			itemstack.damageItem(i, playerIn);
+			playerIn.swingArm(handIn);
 		}
-		return new ActionResult<>(EnumActionResult.SUCCESS, stack);
+		else
+		{
+			worldIn.playSound((EntityPlayer)null, playerIn.posX, playerIn.posY, playerIn.posZ, SoundEvents.ENTITY_BOBBER_THROW, SoundCategory.NEUTRAL, 0.5F, 0.4F / (itemRand.nextFloat() * 0.4F + 0.8F));
+
+			if (!worldIn.isRemote)
+			{
+				EntityFishHook entityfishhook = new EntityFishHook(worldIn, playerIn);
+				int j = EnchantmentHelper.func_191528_c(itemstack);
+
+				if (j > 0)
+				{
+					entityfishhook.func_191516_a(j);
+				}
+
+				int k = EnchantmentHelper.func_191529_b(itemstack);
+
+				if (k > 0)
+				{
+					entityfishhook.func_191517_b(k);
+				}
+
+				worldIn.spawnEntity(entityfishhook);
+			}
+
+			playerIn.swingArm(handIn);
+			playerIn.addStat(StatList.getObjectUseStats(this));
+		}
+
+		return new ActionResult(EnumActionResult.SUCCESS, itemstack);
 	}
 
 }
