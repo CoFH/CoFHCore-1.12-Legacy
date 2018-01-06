11af30e055b3462a5f91d88fee55533f79d3efc6
King Lemming <kinglemming@gmail.com>
2018-01-06 04:45:49 -0500

    Fix a crash w/ Hammers. Yay.
    Signed-off-by: King Lemming <kinglemming@gmail.com>

--- src/main/java/cofh/core/entity/FakePlayerCore.java
+++ src/main/java/cofh/core/entity/FakePlayerCore.java
@@ -3,7 +3,6 @@ package cofh.core.entity;
 import cofh.core.util.helpers.ItemHelper;
 import com.mojang.authlib.GameProfile;
 import net.minecraft.block.state.IBlockState;
-import net.minecraft.init.Items;
 import net.minecraft.init.SoundEvents;
 import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.item.EnumAction;
@@ -20,7 +19,7 @@ import net.minecraftforge.fml.common.FMLCommonHandler;
 
 import java.util.UUID;
 
-public class CoFHFakePlayer extends FakePlayer {
+public class FakePlayerCore extends FakePlayer {
 
 	private static GameProfile NAME = new GameProfile(UUID.fromString("5ae51d0b-e8bc-5a02-09f4-b5dbb05963da"), "[CoFH]");
 
@@ -28,24 +27,24 @@ public class CoFHFakePlayer extends FakePlayer {
 	public ItemStack previousItem = ItemStack.EMPTY;
 	public String myName = "[CoFH]";
 
-	public CoFHFakePlayer(WorldServer world) {
+	public FakePlayerCore(WorldServer world) {
 
 		super(world, NAME);
 		connection = new NetServerHandlerFake(FMLCommonHandler.instance().getMinecraftServerInstance(), this);
 		this.addedToChunk = false;
 	}
 
-	public static boolean isBlockBreakable(CoFHFakePlayer myFakePlayer, World worldObj, BlockPos pos) {
+	public static boolean isBlockBreakable(FakePlayerCore player, World worldObj, BlockPos pos) {
 
 		IBlockState state = worldObj.getBlockState(pos);
 
 		if (state.getBlock().isAir(state, worldObj, pos)) {
 			return false;
 		}
-		if (myFakePlayer == null) {
+		if (player == null) {
 			return state.getBlockHardness(worldObj, pos) > -1;
 		} else {
-			return state.getPlayerRelativeBlockHardness(myFakePlayer, worldObj, pos) > -1;
+			return state.getPlayerRelativeBlockHardness(player, worldObj, pos) > -1;
 		}
 	}
 
@@ -124,13 +123,13 @@ public class CoFHFakePlayer extends FakePlayer {
 	}
 
 	@Override
-	protected void updateItemUse(ItemStack par1ItemStack, int par2) {
+	protected void updateItemUse(ItemStack stack, int par2) {
 
-		if (par1ItemStack.getItemUseAction() == EnumAction.DRINK) {
+		if (stack.getItemUseAction() == EnumAction.DRINK) {
 			this.playSound(SoundEvents.ENTITY_GENERIC_DRINK, 0.5F, this.world.rand.nextFloat() * 0.1F + 0.9F);
 		}
 
-		if (par1ItemStack.getItemUseAction() == EnumAction.EAT) {
+		if (stack.getItemUseAction() == EnumAction.EAT) {
 			this.playSound(SoundEvents.ENTITY_GENERIC_EAT, 0.5F + 0.5F * this.rand.nextInt(2), (this.rand.nextFloat() - this.rand.nextFloat()) * 0.2F + 1.0F);
 		}
 	}
@@ -151,12 +150,6 @@ public class CoFHFakePlayer extends FakePlayer {
 	public float getDefaultEyeHeight() {
 
 		return 1.1F;
-	}
-
-	//@Override TODO
-	public ItemStack getCurrentArmor(int par1) {
-
-		return new ItemStack(Items.DIAMOND_CHESTPLATE);
 	}
 
 }
--- src/main/java/cofh/core/item/tool/ItemHammerCore.java
+++ src/main/java/cofh/core/item/tool/ItemHammerCore.java
@@ -62,9 +62,9 @@ public class ItemHammerCore extends ItemToolCore implements IAOEBreakItem {
 
 		float refStrength = state.getPlayerRelativeBlockHardness(player, world, pos);
 		if (refStrength != 0.0F) {
-			RayTraceResult traceResult = RayTracer.retrace(player);
+			RayTraceResult traceResult = RayTracer.retrace(player, false);
 
-			if (traceResult == null) {
+			if (traceResult == null || traceResult.sideHit == null) {
 				return false;
 			}
 			BlockPos adjPos;
@@ -138,8 +138,8 @@ public class ItemHammerCore extends ItemToolCore implements IAOEBreakItem {
 		ArrayList<BlockPos> area = new ArrayList<>();
 		World world = player.getEntityWorld();
 
-		RayTraceResult traceResult = RayTracer.retrace(player);
-		if (traceResult == null || !canHarvestBlock(world.getBlockState(pos), stack)) {
+		RayTraceResult traceResult = RayTracer.retrace(player, false);
+		if (traceResult == null || traceResult.sideHit == null || !canHarvestBlock(world.getBlockState(pos), stack)) {
 			return ImmutableList.copyOf(area);
 		}
 		BlockPos harvestPos;
