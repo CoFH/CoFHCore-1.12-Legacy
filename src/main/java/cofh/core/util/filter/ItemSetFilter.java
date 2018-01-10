aeed0a75b343d59101d370c990d392a8ced108d6
Florian Hofmair <fhofmair@gmail.com>
2018-01-10 13:37:19 +0100

    Add Filter for item sets

---
+++ src/main/java/cofh/core/util/filter/ItemSetFilter.java
@@ -0,0 +1,92 @@
+package cofh.core.util.filter;
+
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraftforge.common.util.INBTSerializable;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ItemSetFilter implements INBTSerializable<NBTTagCompound> {
+
+    private ItemFilter filter;
+    private int[] stackCounts;
+
+    public ItemSetFilter(int size) {
+
+        filter = new ItemFilter(size);
+        stackCounts = new int[size];
+    }
+
+    public ItemFilter getFilter() {
+
+        return filter;
+    }
+
+    public void setCount(int index, int count) {
+
+        stackCounts[index] = count;
+    }
+
+    public int getCount(int index) {
+
+        return stackCounts[index];
+    }
+
+    public void clear() {
+
+        filter.clear();
+        Arrays.fill(stackCounts, 0);
+    }
+
+    public int getSize() {
+
+        return stackCounts.length;
+    }
+
+    public boolean matches(List<ItemStack> items) {
+
+        for(int i = 0; i < items.size(); i++) {
+            ItemStack stack = items.get(i);
+            if(!filter.matches(stack)) {
+                return false;
+            }
+            if(stack.getCount() < stackCounts[i]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public List<ItemStack> takeSet(List<ItemStack> items) {
+
+        List<ItemStack> ret = new ArrayList<>(stackCounts.length);
+
+        for(int i = 0; i < items.size(); i++) {
+            ItemStack item = items.get(i);
+            ItemStack remainder = item.splitStack(stackCounts[i]);
+            ret.add(remainder);
+        }
+
+        return ret;
+    }
+
+    /* INBTSerializable */
+    @Override
+    public NBTTagCompound serializeNBT() {
+
+        NBTTagCompound nbt = new NBTTagCompound();
+        nbt.setTag("Filter", filter.serializeNBT());
+        nbt.setIntArray("StackCounts", stackCounts);
+        return nbt;
+    }
+
+    @Override
+    public void deserializeNBT(NBTTagCompound nbt) {
+
+        filter.deserializeNBT(nbt.getCompoundTag("Filter"));
+        stackCounts = nbt.getIntArray("StackCounts");
+    }
+}
---
+++ src/main/java/cofh/core/util/filter/ItemSetWrapper.java
@@ -0,0 +1,170 @@
+package cofh.core.util.filter;
+
+import cofh.core.util.helpers.InventoryHelper;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.inventory.IInventory;
+import net.minecraft.item.ItemStack;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.text.ITextComponent;
+
+public class ItemSetWrapper implements IInventory {
+
+    protected final TileEntity tile;
+    protected final ItemSetFilter setFilter;
+
+    public ItemSetWrapper(TileEntity tile, int size) {
+
+        this.tile = tile;
+        this.setFilter = new ItemSetFilter(size);
+        setFilter.deserializeNBT(tile.getTileData().getCompoundTag("Filter"));
+        markDirty();
+    }
+
+    public ItemSetFilter getSetFilter() {
+
+        return setFilter;
+    }
+
+    /* IInventory */
+    @Override
+    public int getSizeInventory() {
+
+        return setFilter.getSize();
+    }
+
+    @Override
+    public boolean isEmpty() {
+
+        return InventoryHelper.isEmpty(setFilter.getFilter().getItems());
+    }
+
+    @Override
+    public ItemStack getStackInSlot(int index) {
+
+        ItemStack stack = setFilter.getFilter().getSlot(index);
+        stack.setCount(setFilter.getCount(index));
+        return stack;
+    }
+
+    @Override
+    public ItemStack decrStackSize(int index, int count) {
+
+        if(count <= 0) {
+            return ItemStack.EMPTY;
+        }
+
+        int stackCount = setFilter.getCount(index);
+        if(stackCount - count <= 0) {
+            ItemStack stack = setFilter.getFilter().getSlot(index);
+            stack.setCount(setFilter.getCount(index));
+
+            setFilter.getFilter().setSlot(index, ItemStack.EMPTY);
+            setFilter.setCount(index, 0);
+
+            return stack;
+        }
+
+        ItemStack stack = setFilter.getFilter().getSlot(index);
+        stack.setCount(setFilter.getCount(index));
+        ItemStack remainder = stack.splitStack(count);
+        setFilter.setCount(index, remainder.getCount());
+        return remainder;
+    }
+
+    @Override
+    public ItemStack removeStackFromSlot(int index) {
+
+        ItemStack stack = setFilter.getFilter().getSlot(index);
+        stack.setCount(setFilter.getCount(index));
+
+        setFilter.getFilter().setSlot(index, ItemStack.EMPTY);
+        setFilter.setCount(index, 0);
+
+        return stack;
+    }
+
+    @Override
+    public void setInventorySlotContents(int index, ItemStack stack) {
+
+        setFilter.getFilter().setSlot(index, stack);
+        setFilter.setCount(index, stack.getCount());
+    }
+
+    @Override
+    public int getInventoryStackLimit() {
+
+        return 64;
+    }
+
+    @Override
+    public void markDirty() {
+
+        tile.getTileData().setTag("Filter", setFilter.serializeNBT());
+        tile.markDirty();
+    }
+
+    @Override
+    public boolean isUsableByPlayer(EntityPlayer player) {
+
+        return true;
+    }
+
+    @Override
+    public void openInventory(EntityPlayer player) {
+
+    }
+
+    @Override
+    public void closeInventory(EntityPlayer player) {
+
+        markDirty();
+    }
+
+    @Override
+    public boolean isItemValidForSlot(int index, ItemStack stack) {
+
+        return true;
+    }
+
+    @Override
+    public int getField(int id) {
+
+        return 0;
+    }
+
+    @Override
+    public void setField(int id, int value) {
+
+    }
+
+    @Override
+    public int getFieldCount() {
+
+        return 0;
+    }
+
+    @Override
+    public void clear() {
+
+        setFilter.clear();
+    }
+
+    /* IWorldNameable */
+    @Override
+    public String getName() {
+
+        return null;
+    }
+
+    @Override
+    public boolean hasCustomName() {
+
+        return false;
+    }
+
+    @Override
+    public ITextComponent getDisplayName() {
+
+        return tile.getDisplayName();
+    }
+}
