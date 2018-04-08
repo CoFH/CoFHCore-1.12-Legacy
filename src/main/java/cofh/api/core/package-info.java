edcb5cc610a6fff68194a20bb55c7b77605fd20f
King Lemming <kinglemming@gmail.com>
2018-04-08 13:16:10 -0400

    Remove superfluous package-info files.
    Signed-off-by: King Lemming <kinglemming@gmail.com>

--- src/main/java/cofh/api/core/package-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * (C) 2014-2017 Team CoFH / CoFH / Cult of the Full Hub
- * http://www.teamcofh.com
- */
-@API (apiVersion = CoFHAPIProps.VERSION, owner = "cofhapi", provides = "cofhapi|core")
-package cofh.api.core;
-
-import cofh.api.CoFHAPIProps;
-import net.minecraftforge.fml.common.API;
