91659f78d24eb3a9443d967b25e3e7a7a0df8e27
King Lemming <kinglemming@gmail.com>
2019-01-05 13:54:21 -0500

    Bump version.
    Signed-off-by: King Lemming <kinglemming@gmail.com>

--- src/main/java/cofh/CoFHCore.java
+++ src/main/java/cofh/CoFHCore.java
@@ -39,7 +39,7 @@ public class CoFHCore {
 	public static final String MOD_ID = "cofhcore";
 	public static final String MOD_NAME = "CoFH Core";
 
-	public static final String VERSION = "4.6.1";
+	public static final String VERSION = "4.6.2";
 	public static final String VERSION_MAX = "4.7.0";
 	public static final String VERSION_GROUP = "required-after:" + MOD_ID + "@[" + VERSION + "," + VERSION_MAX + ");";
 	public static final String UPDATE_URL = "https://raw.github.com/cofh/version/master/" + MOD_ID + "_update.json";
